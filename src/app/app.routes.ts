04050c3567131b09760b3e1c433b9c65f077a783
Casey Gore <gorecasey@gmail.com>
2025-07-01 16:06:00 -0500

    Portfolio Slides

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { AddStudent } from './add-student/add-student';
 import { StudentList } from './student-list/student-list';
 import { EditStudent } from './edit-student/edit-student';
 import { BannerReport } from './banner-report/banner-report';
+import { Portfolio } from './portfolio/portfolio';
 
 
 export const routes: Routes = [
   { path: '', component: StudentList },
   { path: 'add-student', component: AddStudent},
   { path: 'edit-student/:id', component: EditStudent},
-  { path: 'banner-report', component: BannerReport}
+  { path: 'banner-report', component: BannerReport},
+  { path: 'portfolio', component: Portfolio}
 
 ];
